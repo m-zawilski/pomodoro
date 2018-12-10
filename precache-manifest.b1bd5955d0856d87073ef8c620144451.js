bb6fd6dff4f73f6eedcfaa703de3abfa408e157c
Maciej Zawilski <maciejzawilski@Maciejs-MacBook-Pro.local>
2018-12-10 21:50:35 +0100

    Updates

---
+++ precache-manifest.b1bd5955d0856d87073ef8c620144451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bb67a3a95832e3a81c7",
+    "url": "/pomodoro/static/css/main.0b683587.chunk.css"
+  },
+  {
+    "revision": "5bb67a3a95832e3a81c7",
+    "url": "/pomodoro/static/js/main.5bb67a3a.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/pomodoro/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "6de7adfa31c269b165ecffea72dde00d",
+    "url": "/pomodoro/index.html"
+  }
+];
